b9c6e44a0271b7ac36874ddc076f87ca810368b3
Nadav Samet <thesamet@gmail.com>
2015-10-18 16:11:41 -0700

    fix cloning

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-git clone $KUBERNETES_REPO --branch $KUBERNETES_COMMIT
+git clone $KUBERNETES_REPO
+git checkout $KUBERNETES_COMMIT
 KUBE_SKIP_CONFIRMATIONS=y ./kubernetes/build/run.sh hack/build-cross.sh
 cp kubernetes/_output/dockerized/bin/linux/amd64/hyperkube hyperkube/hyperkube
 docker build -t $IMAGE_NAME hyperkube

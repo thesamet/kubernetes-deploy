cb570fcdcab9cad0a1dd8b4e619b5d0fe3ae8713
Nadav Samet <thesamet@gmail.com>
2015-10-18 16:31:23 -0700

    try tags.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 
 git clone $KUBERNETES_REPO
-git checkout $KUBERNETES_COMMIT
+git checkout tags/$KUBERNETES_COMMIT
 KUBE_SKIP_CONFIRMATIONS=y ./kubernetes/build/run.sh hack/build-cross.sh
 cp kubernetes/_output/dockerized/bin/linux/amd64/hyperkube hyperkube/hyperkube
 docker build -t $IMAGE_NAME hyperkube

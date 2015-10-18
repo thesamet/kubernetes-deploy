6dd61d2c685db8c113a3f6329b07d50c671588d7
Nadav Samet <thesamet@gmail.com>
2015-10-18 16:08:13 -0700

    Parameterize kubernetes repo and commit.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-git clone https://github.com/kubernetes/kubernetes.git --branch v1.1.0
+git clone $KUBERNETES_REPO --branch $KUBERNETES_COMMIT
 KUBE_SKIP_CONFIRMATIONS=y ./kubernetes/build/run.sh hack/build-cross.sh
 cp kubernetes/_output/dockerized/bin/linux/amd64/hyperkube hyperkube/hyperkube
 docker build -t $IMAGE_NAME hyperkube
